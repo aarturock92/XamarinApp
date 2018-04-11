564e1d8c0cf02a2e80f5528d7792e3856426dea5
arturo <aarturock92@gmail.com>
2018-04-10 23:16:20 -0500

    Creación del modelo User

---
+++ XamarinExample/XamarinExample/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace XamarinExample.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+
+        public string Token { get; set; }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string AvatarUrl { get; set; }        
+    }
+}
