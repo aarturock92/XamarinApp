b2187d13f6b3b39e5a3941201ce940fdc697486a
arturo <aarturock92@gmail.com>
2018-04-10 23:38:32 -0500

    Creación del modelo de datos Hotel, Room, Service (se encuentran relacionados)

---
+++ XamarinExample/XamarinExample/Models/Room.cs
@@ -0,0 +1,10 @@
+﻿namespace XamarinExample.Models
+{
+    public class Room
+    {
+        public int RoomId { get; set; }
+        public string RoomName { get; set; }
+        public int  RoomType { get; set; }
+        public int Quantity { get; set; }
+    }
+}
--- XamarinExample/XamarinExample/Models/User.cs
+++ XamarinExample/XamarinExample/Models/User.cs
@@ -3,15 +3,10 @@
     public class User
     {
         public string Id { get; set; }
-
         public string Token { get; set; }
-
         public string Name { get; set; }
-
         public string LastName { get; set; }
-
         public string Email { get; set; }
-
         public string AvatarUrl { get; set; }        
     }
 }
