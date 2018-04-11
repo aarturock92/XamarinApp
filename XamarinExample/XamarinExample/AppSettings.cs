4829dd166fb04258312cb8e766433cb766677279
arturo <aarturock92@gmail.com>
2018-04-10 23:52:25 -0500

    Se agrego clase que contiene los endpoints de los servicios azure

---
+++ XamarinExample/XamarinExample/AppSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace XamarinExample
+{
+    public static class AppSettings
+    {
+        private const string DefaultBookingEndpoint = "YOUR_BOOKING_ENDPOINT";
+        private const string DefaultHotelsEndpoint = "YOUR_HOTELS_ENDPOINT";
+
+    }
+}
---
+++ XamarinExample/XamarinExample/Services/Hotel/IHotelService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace XamarinExample.Services.Hotel
+{
+    public interface IHotelService
+    {
+        Task<IEnumerable<Models.Hotel>> SearchAsync(int cityId);
+
+        Task<IEnumerable<Models.Hotel>> SearchAsync(int cityId, int rating, int minPrice, int maxPrice);
+
+        Task<IEnumerable<Models.Hotel>> GetMostVisitedAsync();
+
+        Task<Models.Hotel> GetHotelByIdAsync(int id);
+
+        Task<IEnumerable<Models.Service>> GetHotelServiceAsync();
+
+        Task<IEnumerable<Models.Service>> GetRoomServicesAsync();
+
+    }
+}
